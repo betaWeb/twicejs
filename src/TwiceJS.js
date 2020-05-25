49f40aecd09285f881cf27cb613852a16912f45e
betaWeb <cyprien.glepin@gmail.com>
2020-05-25 16:22:38 +0200

    feat(tests)

--- src/TwiceJS.js
+++ src/TwiceJS.js
@@ -4,255 +4,262 @@
  */
 export default class TwiceJS {
 
-    /**
-     * @returns {Object.<String, String>}
-     */
-    static get DEFAULT_OPTIONS() {
-        return {
-            count_key: '_count',
-            key_encoder: TwiceJS.ENCODERS.JSON
-        }
-    }
+	/**
+	 * @returns {Object.<String, String>}
+	 */
+	static get DEFAULT_OPTIONS() {
+		return {
+			count_key: '_count',
+			key_encoder: TwiceJS.ENCODERS.JSON
+		}
+	}
 
-    /**
-     * @returns {Object.<String, String>}
-     */
-    static get ENCODERS() {
-        return {
-            JSON: 'json',
-            BASE_64: 'base64'
-        }
-    }
+	/**
+	 * @returns {Object.<String, String>}
+	 */
+	static get ENCODERS() {
+		return {
+			JSON: 'json',
+			BASE_64: 'base64'
+		}
+	}
 
-    /**
-     * @returns {Array}
-     */
-    get entries() {
-        return Array.from(this.keys.map(this._decodeKey.bind(this)))
-    }
+	/**
+	 * @returns {Array}
+	 */
+	get entries() {
+		return Array.from(this.keys.map(this._decodeKey.bind(this)))
+	}
 
-    get encodedEntries() {
-        return Array.from(this.keys)
-    }
+	get encodedEntries() {
+		return Array.from(this.keys)
+	}
 
-    /**
-     * @returns {Object[] | Array[]}
-     */
-    get withCount() {
-        return this.keys.reduce((acc, key) => {
-            const count = this._dataset.get(key)
-            const item = this._decodeKey(key)
+	/**
+	 * @returns {Object[] | Array[]}
+	 */
+	get withCount() {
+		return this.keys.reduce((acc, key) => {
+			const count = this._dataset.get(key)
+			const item = this._decodeKey(key)
 
-            if (item.constructor !== Object)
-                return [...acc, [item, count]]
+			if (item.constructor !== Object)
+				return [...acc, [item, count]]
 
-            return [
-                ...acc,
-                {
-                    ...item,
-                    ...{ [this.options.count_key]: count }
-                }
-            ]
-        }, [])
-    }
+			return [
+				...acc,
+				{
+					...item,
+					...{ [this.options.count_key]: count }
+				}
+			]
+		}, [])
+	}
 
-    /**
-     * @returns {Array}
-     */
-    get keys() {
-        return [...this._dataset.keys()]
-    }
+	/**
+	 * @returns {Array}
+	 */
+	get keys() {
+		return [...this._dataset.keys()]
+	}
 
-    /**
-     * @returns {Map}
-     */
-    get rawMap() {
-        return this._dataset
-    }
+	/**
+	 * @returns {Map}
+	 */
+	get rawMap() {
+		return this._dataset
+	}
 
-    /**
-     * @param {Object} options 
-     */
-    constructor(options = {}) {
-        this.options = { ...TwiceJS.DEFAULT_OPTIONS, ...options }
-        this.clear()
-    }
+	/**
+	 * @param {Object} options 
+	 */
+	constructor(options = {}) {
+		this.options = { ...TwiceJS.DEFAULT_OPTIONS, ...options }
+		this.clear()
+	}
 
-    /**
-     * @returns {Boolean}
-     */
-    isEmpty() {
-        return this.keys.length === 0 || this._dataset.size === 0
-    }
+	/**
+	 * @returns {Boolean}
+	 */
+	isEmpty() {
+		return this.keys.length === 0 || this._dataset.size === 0
+	}
 
-    /**
-     * @returns {TwiceJS}
-     */
-    clear() {
-        this._dataset = new Map()
-        return this
-    }
+	/**
+	 * @returns {TwiceJS}
+	 */
+	clear() {
+		this._dataset = new Map()
+		return this
+	}
 
-    /**
-     * @param {Array|*} data
-     * @returns {TwiceJS}
-     */
-    append(data) {
-        return this._process(data, item => {
-            const key = this._keyify(item)
-            let count = this._dataset.get(key) || 0
+	/**
+	 * @param {Array|*} data
+	 * @returns {TwiceJS}
+	 */
+	append(data) {
+		return this._process(data, item => {
+			const key = this._keyify(item)
+			let count = this._dataset.get(key) || 0
 
-            this._dataset.set(key, count += 1)
-        })
-    }
+			this._dataset.set(key, count += 1)
+		})
+	}
 
-    /**
-     * @param {Array|*} oldKey
-     * @param {Array|*} newKey
-     * @returns {TwiceJS}
-     */
-    replace(oldKey, newKey) {
-        oldKey = this._keyify(oldKey)
-        newKey = this._keyify(newKey)
+	/**
+	 * @param {Array|*} oldKey
+	 * @param {Array|*} newKey
+	 * @returns {TwiceJS}
+	 */
+	replace(oldKey, newKey) {
+		oldKey = this._keyify(oldKey)
+		newKey = this._keyify(newKey)
 
-        if (!this._dataset.has(oldKey))
-            throw new Error(`[Err] TwiceJS.set :: key '${oldKey}' doesn't exists on dataset`)
+		if (!this._dataset.has(oldKey))
+			throw new Error(`[Err] TwiceJS.set :: key '${oldKey}' doesn't exists on dataset`)
 
-        const oldCount = this._dataset.get(oldKey)
-        const newKeyExists = this._dataset.has(newKey)
+		const oldCount = this._dataset.get(oldKey)
+		const newKeyExists = this._dataset.has(newKey)
 
-        this
-            .remove(oldKey)
-            .append(newKey)
+		this
+			.remove(oldKey)
+			.append(newKey)
 
-        if (!newKeyExists)
-            this._dataset.set(newKey, oldCount)
+		if (!newKeyExists)
+			this._dataset.set(newKey, oldCount)
 
-        return this
-    }
+		return this
+	}
 
-    /**
-     * @param {Array|*} data
-     * @returns {TwiceJS}
-     */
-    remove(data) {
-        return this._process(data, item => {
-            const key = this._keyify(item)
+	/**
+	 * @param {Array|*} data
+	 * @returns {TwiceJS}
+	 */
+	remove(data) {
+		return this._process(data, item => {
+			const key = this._keyify(item)
 
-            if (this._dataset.has(key))
-                this._dataset.delete(key)
-        })
-    }
+			if (this._dataset.has(key))
+				this._dataset.delete(key)
+		})
+	}
 
-    /**
-     * Get if an item exists on dataset
-     * 
-     * @param {*} item 
-     * @returns Boolean
-     */
-    has(item) {
-        if (!Array.isArray(item) && item.constructor !== Object)
-            return this.keys.includes(item)
-        return this._dataset.has(item)
-    }
+	/**
+	 * Get if an item exists on dataset
+	 * 
+	 * @param {*} item 
+	 * @returns Boolean
+	 */
+	has(item) {
+		if (this.isEmpty()) return false
 
-    /**
-     * Get an item occurrences count
-     * 
-     * @param {*} item 
-     * @returns {Number}
-     */
-    occurrence(item) {
-        const key = this._keyify(item)
-        return this._dataset.get(key) || 0
-    }
+		const encoded_item = this._keyify(item)
 
-    /**
-     * Encode an entry according the defined key_encoder option
-     * 
-     * @param {*} item 
-     * @returns {String}
-     */
-    encode(item) {
-        return this._keyify(item)
-    }
+		return (
+			this.encodedEntries.includes(item) ||
+			this.encodedEntries.includes(encoded_item) ||
+			this.entries.includes(item) ||
+			this.entries.includes(encoded_item)
+		)
+	}
 
-    /**
-     * Decode an entry according the defined key_encoder option
-     * 
-     * @param {String} key
-     * @returns {*}
-     */
-    decode(key) {
-        return this._decodeKey(key)
-    }
+	/**
+	 * Get an item occurrences count
+	 * 
+	 * @param {*} item 
+	 * @returns {Number}
+	 */
+	occurrence(item) {
+		const key = this._keyify(item)
+		return this._dataset.get(key) || 0
+	}
 
-    /**
-     * @param {Array} data
-     * @param {Function} process
-     * @returns {TwiceJS}
-     *
-     * @private
-     */
-    _process(data, process) {
-        if (!Array.isArray(data))
-            data = [data]
+	/**
+	 * Encode an entry according the defined key_encoder option
+	 * 
+	 * @param {*} item 
+	 * @returns {String}
+	 */
+	encode(item) {
+		return this._keyify(item)
+	}
 
-        data = this._normalize(data)
-        data.forEach(process.bind(this))
-        return this
-    }
+	/**
+	 * Decode an entry according the defined key_encoder option
+	 * 
+	 * @param {String} key
+	 * @returns {*}
+	 */
+	decode(key) {
+		return this._decodeKey(key)
+	}
 
-    /**
-     * @param {*} item 
-     * @returns {String}
-     *
-     * @private
-     */
-    _keyify(item) {
-        if (!Array.isArray(item) && item.constructor !== Object)
-            return item.toString()
+	/**
+	 * @param {Array} data
+	 * @param {Function} process
+	 * @returns {TwiceJS}
+	 *
+	 * @private
+	 */
+	_process(data, process) {
+		if (!Array.isArray(data))
+			data = [data]
 
-        try {
-            item = JSON.stringify(item)
+		data = this._normalize(data)
+		data.forEach(process.bind(this))
+		return this
+	}
 
-            if (this.options.key_encoder === TwiceJS.ENCODERS.BASE_64)
-                item = btoa(item)
-        } catch (e) {
-            throw new Error(`[Err] TwiceJS._keyify :: error on keyify item ${item}\n${e}`)
-        }
-        return item
-    }
+	/**
+	 * @param {*} item 
+	 * @returns {String}
+	 *
+	 * @private
+	 */
+	_keyify(item) {
+		if (!Array.isArray(item) && item.constructor !== Object)
+			return item.toString()
 
-    /**
-     * @param {String} key
-     * @returns {*}
-     *
-     * @private
-     */
-    _decodeKey(key) {
-        try {
-            if (this.options.key_encoder === TwiceJS.ENCODERS.BASE_64)
-                key = atob(key)
+		try {
+			item = JSON.stringify(item)
 
-            return JSON.parse(key)
-        } catch (err) {
-            return key;
-        }
-    }
+			if (this.options.key_encoder === TwiceJS.ENCODERS.BASE_64)
+				item = btoa(item)
+		} catch (e) {
+			throw new Error(`[Err] TwiceJS._keyify :: error on keyify item ${item}\n${e}`)
+		}
+		return item
+	}
 
-    /**
-     * @param {Array|Object|*} data
-     * @returns {Array|Object|*}
-     *
-     * @private
-     */
-    _normalize(data) {
-        try {
-            return JSON.parse(JSON.stringify(data))
-        } catch (e) {
-            return data
-        }
-    }
+	/**
+	 * @param {String} key
+	 * @returns {*}
+	 *
+	 * @private
+	 */
+	_decodeKey(key) {
+		try {
+			if (this.options.key_encoder === TwiceJS.ENCODERS.BASE_64)
+				key = atob(key)
+
+			return JSON.parse(key)
+		} catch (err) {
+			return key;
+		}
+	}
+
+	/**
+	 * @param {Array|Object|*} data
+	 * @returns {Array|Object|*}
+	 *
+	 * @private
+	 */
+	_normalize(data) {
+		try {
+			return JSON.parse(JSON.stringify(data))
+		} catch (e) {
+			return data
+		}
+	}
 
 }
--- test/TwiceJS.spec.js
+++ test/TwiceJS.spec.js
@@ -1,20 +1,159 @@
 import TwiceJS from '../index'
 
+function ArrayDataset(options = {}) {
+	const iterable = [1, 1, 2, 1, 3, 3]
+	const dataset = (new TwiceJS(options)).append(iterable)
+	return { iterable, dataset }
+}
+
+function objectDataset(options = {}) {
+	const iterable = [{ id: 1 }, { id: 1 }, { id: 2 }, { id: 1 }, { id: 3 }, { id: 3 }]
+	const dataset = (new TwiceJS(options)).append(iterable)
+	return { iterable, dataset }
+}
+
 describe('TwiceJS', () => {
 
-    it('Should instantiate TwiceJS class correctly', () => {
-        const dataset = new TwiceJS()
-        expect(dataset).toBeInstanceOf(TwiceJS)
-    })
+	describe('Basics', () => {
 
-    it('Should dedupe simple array', () => {
-        const iterable = [1, 1, 2, 1, 3, 3]
-        const dataset = new TwiceJS()
+		it('Should instantiate TwiceJS class correctly', () => {
+			const dataset = new TwiceJS()
+			expect(dataset).toBeInstanceOf(TwiceJS)
+		})
 
-        dataset.append(iterable)
+		it('Should dedupe simple array', () => {
+			const { iterable, dataset } = ArrayDataset()
 
-        expect(iterable.length).toEqual(6)
-        expect(dataset.entries.length).toEqual(3)
-    })
+			expect(iterable.length).toEqual(6)
+			expect(dataset.isEmpty()).toBeFalse()
+			expect(dataset.entries.length).toEqual(3)
+		})
+
+		it('Should dedupe complex array', () => {
+			const { iterable, dataset } = objectDataset()
+
+			expect(iterable.length).toEqual(6)
+			expect(dataset.isEmpty()).toBeFalse()
+			expect(dataset.entries.length).toEqual(3)
+		})
+
+		it('should verify if an entry exists on a simple array', () => {
+			const { dataset } = ArrayDataset()
+
+			expect(dataset.has(1)).toBeTrue()
+			expect(dataset.has(7)).toBeFalse()
+		})
+
+		it('should verify if an entry exists on a complex array', () => {
+			const { dataset } = objectDataset()
+
+			expect(dataset.has({ id: 1 })).toBeTrue()
+			expect(dataset.has({ id: 7 })).toBeFalse()
+		})
+
+		it('Should replace an element', () => {
+			const { dataset } = objectDataset()
+
+			expect(dataset.entries).not.toContainValue({ id: 4 })
+			expect(dataset.entries).toContainValue({ id: 1 })
+
+			dataset.replace({ id: 1 }, { id: 4 })
+
+			expect(dataset.entries).not.toContainValue({ id: 1 })
+			expect(dataset.entries).toContainValue({ id: 4 })
+		})
+
+		it('Should remove an element', () => {
+			const { dataset } = objectDataset()
+
+			expect(dataset.entries).toContainValue({ id: 1 })
+
+			dataset.remove({ id: 1 })
+
+			expect(dataset.entries).not.toContainValue({ id: 1 })
+		})
+
+		it('Should count occurences of an element', () => {
+			const { dataset } = objectDataset()
+
+			expect(dataset.occurrence({ id: 1 })).toEqual(3)
+			expect(dataset.occurrence({ id: 2 })).toEqual(1)
+			expect(dataset.occurrence({ id: 3 })).toEqual(2)
+		})
+
+		it('Should returns entries with occurrences count key', () => {
+			const { dataset } = objectDataset()
+
+			expect(dataset.withCount[0].id).toEqual(1)
+			expect(dataset.withCount[0]._count).toEqual(3)
+
+			expect(dataset.withCount[1].id).toEqual(2)
+			expect(dataset.withCount[1]._count).toEqual(1)
+
+			expect(dataset.withCount[2].id).toEqual(3)
+			expect(dataset.withCount[2]._count).toEqual(2)
+		})
+
+	})
+
+	describe('Encoding/Decoding', () => {
+
+		it('Should JSON encode entries correctly', () => {
+			const { dataset } = objectDataset()
+
+			expect(dataset.encodedEntries).toContainAllValues(['{"id":1}', '{"id":2}', '{"id":3}'])
+		})
+
+		it('Should base64 encode entries correctly ', () => {
+			const { dataset } = objectDataset({
+				key_encoder: TwiceJS.ENCODERS.BASE_64
+			})
+
+			expect(dataset.encodedEntries).toContainAllValues(['eyJpZCI6MX0=', 'eyJpZCI6Mn0=', 'eyJpZCI6M30='])
+		})
+
+		it('Should JSON decode entries correctly', () => {
+			const { dataset } = objectDataset()
+
+			expect(dataset.entries).toContainAllValues([{ id: 1 }, { id: 2 }, { id: 3 }])
+		})
+
+		it('Should base64 decode entries correctly ', () => {
+			const { dataset } = objectDataset({
+				key_encoder: TwiceJS.ENCODERS.BASE_64
+			})
+
+			expect(dataset.entries).toContainAllValues([{ id: 1 }, { id: 2 }, { id: 3 }])
+		})
+
+		it('Should JSON encode entry correctly', () => {
+			const { dataset } = objectDataset()
+
+			expect(dataset.encode({ id: 1 })).toEqual('{"id":1}')
+		})
+
+		it('Should base64 encode entry correctly ', () => {
+			const { dataset } = objectDataset({
+				key_encoder: TwiceJS.ENCODERS.BASE_64
+			})
+
+			expect(dataset.encode({ id: 1 })).toEqual('eyJpZCI6MX0=')
+		})
+
+		it('Should JSON decode entry correctly', () => {
+			const { dataset } = objectDataset()
+
+			expect(dataset.decode('{"id":1}')).toEqual({ id: 1 })
+		})
+
+		it('Should base64 decode entry correctly ', () => {
+			const { dataset } = objectDataset({
+				key_encoder: TwiceJS.ENCODERS.BASE_64
+			})
+
+			expect(dataset.decode('eyJpZCI6MX0=')).toEqual({ id: 1 })
+		})
+
+	})
 
 })
